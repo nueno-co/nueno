cabf3e5e3f3b6d98631427cf9e4a60cdc42c6cb3
danieltonel <danieltonel.xyz@gmail.com>
2022-03-06 03:41:25 +0100

    simplify response codes

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -10,7 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     const response: ResponseParams = await entity.create(req.body);
-    return res.status(201).json(response);
+    return res.status(200).json(response);
   } catch (error) {
     if (error instanceof HttpError) return res.status(error.code).json(error.message);
     throw error;
--- pages/api/jobs/list.ts
+++ pages/api/jobs/list.ts
@@ -14,7 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     const response: ResponseParams = await entity.list(session.user.id);
-    return res.status(201).json(response);
+    return res.status(200).json(response);
   } catch (error) {
     if (error instanceof HttpError) return res.status(error.code).json(error.message);
     throw error;
