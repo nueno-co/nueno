07aeff328092a496175e4ad731545f800dcb4175
danieltonel <danieltonel.xyz@gmail.com>
2022-03-06 02:26:29 +0100

    Clean up code

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,18 +1,18 @@
-import { SignupResponseParams } from "@api-contracts/auth/signup";
+import { SignupResponseParams as ResponseParams } from "@api-contracts/auth/signup";
 import UserEntity from "@business-logic/User";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") return;
 
-  const userEntity = new UserEntity();
+  const entity = new UserEntity();
 
   try {
-    const response: SignupResponseParams = await userEntity.create(req.body);
+    const response: ResponseParams = await entity.create(req.body);
     return res.status(201).json(response);
   } catch (error) {
-    const errorCode = userEntity.error?.code;
-    const errorMessage = userEntity.error?.message;
+    const errorCode = entity.error?.code;
+    const errorMessage = entity.error?.message;
     if (errorCode && errorMessage) return res.status(errorCode).json(errorMessage);
 
     throw error;
--- pages/api/jobs/list.ts
+++ pages/api/jobs/list.ts
@@ -1,4 +1,4 @@
-import { JobsListResponseParams } from "@api-contracts/jobs/list";
+import { JobsListResponseParams as ResponseParams } from "@api-contracts/jobs/list";
 import JobEntity from "@business-logic/Job";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
@@ -9,14 +9,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const session = await getSession({ req });
   if (!session) return res.status(401).json({ message: "Not authenticated" });
 
-  const jobEntity = new JobEntity();
+  const entity = new JobEntity();
 
   try {
-    const response: JobsListResponseParams = await jobEntity.list(session.user.id);
+    const response: ResponseParams = await entity.list(session.user.id);
     return res.status(201).json(response);
   } catch (error) {
-    const errorCode = jobEntity.error?.code;
-    const errorMessage = jobEntity.error?.message;
+    const errorCode = entity.error?.code;
+    const errorMessage = entity.error?.message;
     if (errorCode && errorMessage) return res.status(errorCode).json(errorMessage);
 
     throw error;
