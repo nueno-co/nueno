a6d4daf653304f7810bca5f44ba77c89695ee815
danieltonel <danieltonel.xyz@gmail.com>
2022-03-29 20:21:09 +0200

    add request params type to create endpoint

--- pages/api/jobs/create.ts
+++ pages/api/jobs/create.ts
@@ -1,4 +1,7 @@
-import { JobsCreateResponseParams as ResponseParams } from "@api-contracts/jobs/create";
+import {
+  JobsCreateRequestParams as RequestParams,
+  JobsCreateResponseParams as ResponseParams,
+} from "@api-contracts/jobs/create";
 import JobEntity from "@business-logic/Job";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
@@ -14,7 +17,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const entity = new JobEntity();
 
   try {
-    const response: ResponseParams = await entity.create(req.body, session.user.id);
+    const requestBody: RequestParams = req.body;
+    const response: ResponseParams = await entity.create(requestBody, session.user.id);
     return res.status(200).json(response);
   } catch (error) {
     if (error instanceof HttpError) return res.status(error.code).json(error.message);
