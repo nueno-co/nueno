81d83dbe25fcffb05fafea8459583f42f4de8300
Jean Francois Munyaneza <mjeanfrancois360@gmail.com>
2022-04-26 16:24:24 +0200

    Add login & regiter page UI

--- pages/jobs/new/[id]/application-form.tsx
+++ pages/jobs/new/[id]/application-form.tsx
@@ -93,7 +93,7 @@ export default function JobsNew() {
                     <button
                       type="button"
                       onClick={submit}
-                      className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                      className="inline-flex items-center px-4 py-2 mt-3 font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm mt-text-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
                       Add field
                     </button>
                   </div>
