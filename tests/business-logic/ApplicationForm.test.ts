624fa295f5d77d80c2deb3d0430519c38beb0ccb
ISHIMWE Divin Fiston <divinfiston1@gmail.com>
2022-04-26 16:08:38 +0200

    Testing application Form

---
+++ tests/business-logic/ApplicationForm.test.ts
@@ -0,0 +1,133 @@
+import ApplicationFormEntity from "@business-logic/ApplicationForm";
+import { Field, Job } from "@prisma/client";
+
+import prisma from "@helpers/prisma";
+import { createJob } from "@helpers/tests/createJob";
+import { minimalSetup } from "@helpers/tests/setup";
+import { teardown } from "@helpers/tests/teardown";
+
+describe("ApplicationForm", () => {
+  beforeEach(async () => {
+    await teardown();
+  });
+
+  describe("#create", () => {
+    it("creates applicationForm", async () => {
+      const { user } = await minimalSetup();
+      await Promise.all([createJob(user.companyId)]);
+      const job = (await prisma.job.findFirst({
+        where: {
+          companyId: user.companyId,
+        },
+      })) as Job;
+
+      const requestParams = {
+        fields: [
+          {
+            type: "SHORT_TEXT",
+            label: "Github user URL",
+          },
+        ],
+        jobUid: job.uid,
+      };
+
+      const entity = new ApplicationFormEntity();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const result = await entity.create(requestParams, user.id);
+
+      const applicationForm = (await prisma.field.findFirst({
+        where: { jobId: job.id },
+      })) as Field;
+      expect(applicationForm.label).toBe(requestParams.fields[0].label);
+      expect(applicationForm.type).toBe(requestParams.fields[0].type);
+    });
+
+    it("throws error if user was not found", async () => {
+      const { user } = await minimalSetup();
+      await Promise.all([createJob(user.companyId)]);
+      const job = (await prisma.job.findFirst({
+        where: {
+          companyId: user.companyId,
+        },
+      })) as Job;
+
+      const requestParams = {
+        fields: [
+          {
+            type: "SHORT_TEXT",
+            label: "Github user URL",
+          },
+        ],
+        jobUid: job.uid,
+      };
+      console.log(job);
+
+      const nonExistingUserId = 99999;
+      const entity = new ApplicationFormEntity();
+
+      await expect(async () => {
+        await entity.create(requestParams, nonExistingUserId);
+      }).rejects.toThrowError("User Not found");
+    });
+  });
+
+  describe("#list", () => {
+    it("lists all applicationForms of a job", async () => {
+      const { user } = await minimalSetup();
+      await Promise.all([createJob(user.companyId)]);
+      const job = (await prisma.job.findFirst({
+        where: {
+          companyId: user.companyId,
+        },
+      })) as Job;
+
+      const params = {
+        fields: [
+          {
+            type: "SHORT_TEXT",
+            label: "Github user URL",
+          },
+          {
+            type: "LONG_TEXT",
+            label: "Describe Yourself",
+          },
+        ],
+        jobUid: job.uid,
+      };
+
+      const entity = new ApplicationFormEntity();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const result = await entity.create(params, user.id);
+      const fieldsList = await entity.list(user.id, job.id);
+
+      expect(fieldsList.length).toBe(2);
+    });
+
+    it("throws error if user was not found", async () => {
+      const { user } = await minimalSetup();
+      await Promise.all([createJob(user.companyId)]);
+      const job = (await prisma.job.findFirst({
+        where: {
+          companyId: user.companyId,
+        },
+      })) as Job;
+
+      const nonExistingUserId = 99999;
+      const entity = new ApplicationFormEntity();
+
+      await expect(async () => {
+        await entity.list(nonExistingUserId, job.id);
+      }).rejects.toThrowError("User Not found");
+    });
+    it("throws error if Job was not found", async () => {
+      const { user } = await minimalSetup();
+
+      const nonExistingJobId = 999999;
+      const entity = new ApplicationFormEntity();
+
+      await expect(async () => {
+        await entity.list(user.id, nonExistingJobId);
+      }).rejects.toThrowError("Job Not found");
+    });
+  });
+});
