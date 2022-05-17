3139a40f0b46cd4c7253805ad47fedbd77ae4135
Mahdi <muhdmahdia36@gmail.com>
2022-05-17 18:50:53 +0100

    console log removed in signup page

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -42,7 +42,6 @@ export default function Signup() {
 
   async function handleSubmit(e: React.SyntheticEvent) {
     e.preventDefault();
-    console.log(user);
 
     if (isSubmitting) {
       return;
